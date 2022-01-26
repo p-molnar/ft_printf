451fa2ea37174cd7b1c5069fc43c93e884f59a81
Peter Molnar <pmolnar@f1r4s12.codam.nl>
2022-01-26 11:33:04 +0100

    removed duplicate functions

--- utils/fmt_arg_utils.c
+++ utils/fmt_arg_utils.c
@@ -1,48 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   print_arg_utils.c                                  :+:    :+:            */
+/*   fmt_arg_utils.c                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/20 19:37:00 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/01/21 14:24:37 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/01/26 11:32:13 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 #include "../libft/libft.h"
 #include <stdio.h>
-
-size_t	add_width(size_t n)
-{
-	size_t	i;
-	size_t	printed_char_count;
-
-	i = 0;
-	printed_char_count = 0;
-	while (i < n)
-	{
-		printed_char_count += ft_put_char(CHAR_SPACE);
-		i++;
-	}
-	return (printed_char_count);
-}
-
-size_t	add_precision(size_t n)
-{
-	size_t	i;
-	size_t	printed_char_count;
-
-	i = 0;
-	printed_char_count = 0;
-	while (i < n)
-	{
-		printed_char_count += ft_put_char(CHAR_ZERO);
-		i++;
-	}
-	return (printed_char_count);
-}
 
 size_t	calc_precision(t_fmt *fmt, long long n, t_print_fmt p_fmt)
 {
@@ -70,24 +40,4 @@ size_t	calc_width(t_fmt *fmt, long long n, t_print_fmt p_fmt)
 	else if (fmt->specifier == 'p')
 		width = get_max(2, width - 2, 0);
 	return (width);
-}
-
-size_t	add_prefix(t_fmt *fmt, long long n)
-{
-	size_t	printed_char_count;	
-
-	printed_char_count = 0;
-	if (fmt->flags.space && n >= 0)
-		printed_char_count += ft_put_char(CHAR_SPACE);
-	else if (n < 0 && fmt->specifier != 'p')
-		printed_char_count += ft_put_char(CHAR_MINUS);
-	else if (fmt->flags.plus && n >= 0)
-		printed_char_count += ft_put_char(CHAR_PLUS);
-	else if (fmt->flags.hash && n != 0 && fmt->specifier == 'x')
-		printed_char_count += ft_put_str(HEX_PREFIX_LOWER, 2);
-	else if (fmt->flags.hash && n != 0 && fmt->specifier == 'X')
-		printed_char_count += ft_put_str(HEX_PREFIX_UPPER, 2);
-	else if (fmt->specifier == 'p')
-		printed_char_count += ft_put_str(HEX_PREFIX_LOWER, 2);
-	return (printed_char_count);
 }
